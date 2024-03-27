8ddc75fc6c6c10974b1d4219a8fb69d8c2aae0b3
rjdbcm <rjdbcm@mail.umkc.edu>
2024-03-26 19:15:18 -0500

    switch to single python version action
    Signed-off-by: rjdbcm <rjdbcm@mail.umkc.edu>

--- action.sh
+++ action.sh
@@ -4,7 +4,6 @@ set -e
 
 # Copy inputs into correctly-named environment variables
 export GH_TOKEN="${INPUT_GITHUB_TOKEN}"
-export PYTHON_VERSION="${INPUT_PYTHON_VERSION:=$1}"
 
 # Change to configured directory
 cd "${INPUT_DIRECTORY}"
@@ -13,24 +12,4 @@ cd "${INPUT_DIRECTORY}"
 # and https://github.com/actions/runner-images/issues/6775#issuecomment-1410270956
 git config --system --add safe.directory "*"
 
-case $PYTHON_VERSION in
-
-"3.12")
-    export PATH=/root/.pyenv/versions/3.12.2/bin:${PATH}
-    ;;
-
-"3.11")
-    export PATH=/root/.pyenv/versions/3.11.8/bin:${PATH}
-    ;;
-
-"3.10")
-    export PATH=/root/.pyenv/versions/3.10.13/bin:${PATH}
-    ;;
-
-esac
-
-echo Install pyc_wheel for Python ${PYTHON_VERSION}
-python"${PYTHON_VERSION}" -m pip install .
-
-echo Run pyc_wheel for Python ${PYTHON_VERSION}
-python"${PYTHON_VERSION}" -m pyc_wheel "${INPUT_FILENAME}"
+python3.10 -m pyc_wheel "${INPUT_FILENAME}"
